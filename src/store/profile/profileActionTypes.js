675229affaa10f317ed2f2c7b4a54d47d0f2fe60
jotacib3 <jotacib3@gmail.com>
2021-11-22 15:17:17 -0500

    refactor(src/store): create `profileActionTypes.js`

---
+++ src/store/profile/profileActionTypes.js
@@ -0,0 +1,6 @@
+export const profileNamespace = 'profile';
+
+export const ProfileActionTypes = {
+  FETCH_AND_SAVE_PROFILE: `${profileNamespace}/FETCH_AND_SAVE_PROFILE`,
+  UPDATE_PROFILE: `${profileNamespace}/UPDATE_PROFILE`,
+};
