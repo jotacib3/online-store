5e5a62e6d14f1de937a1ddaaaeecf45c5e8885c5
jotacib3 <jotacib3@gmail.com>
2021-11-22 12:20:32 -0500

    refactor(src/app): change titleTemplate and defaultTitle in `index.jsx`

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -15,11 +15,8 @@ import { AppRoutes } from './routes';
 export function App() {
   return (
     <BrowserRouter>
-      <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
-      >
-        <meta name="description" content="A React Boilerplate application" />
+      <Helmet titleTemplate="%s - Online Store" defaultTitle="Online Store">
+        <meta name="description" content="A Online Store application" />
       </Helmet>
       <AppRoutes />
       <GlobalStyle />
