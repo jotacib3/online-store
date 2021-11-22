16f0800114895c0a4941c67730828c4b42c25cf6
jotacib3 <jotacib3@gmail.com>
2021-11-22 09:55:47 -0500

    refactor(src/app): create default component `index.jsx`

---
+++ src/app/index.jsx
@@ -0,0 +1,28 @@
+/**
+ *
+ * App
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from '../styles/global-styles';
+import { AppRoutes } from './routes';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Helmet
+        titleTemplate="%s - React Boilerplate"
+        defaultTitle="React Boilerplate"
+      >
+        <meta name="description" content="A React Boilerplate application" />
+      </Helmet>
+      <AppRoutes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
