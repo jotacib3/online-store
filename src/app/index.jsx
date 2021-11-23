43f21dd8d5e0cd6eae3a5965ddbc592ffc18e29b
jotacib3 <jotacib3@gmail.com>
2021-11-22 19:24:52 -0500

    refactor(src/app): add SnackbarProvider component

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -9,17 +9,20 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 import { GlobalStyle } from '../styles/global-styles';
 import { AppRoutes } from './routes';
 
 export function App() {
   return (
     <BrowserRouter>
-      <Helmet titleTemplate="%s - Online Store" defaultTitle="Online Store">
-        <meta name="description" content="A Online Store application" />
-      </Helmet>
-      <AppRoutes />
-      <GlobalStyle />
+      <SnackbarProvider dense maxSnack={3}>
+        <Helmet titleTemplate="%s - Online Store" defaultTitle="Online Store">
+          <meta name="description" content="A Online Store application" />
+        </Helmet>
+        <AppRoutes />
+        <GlobalStyle />
+      </SnackbarProvider>
     </BrowserRouter>
   );
 }
