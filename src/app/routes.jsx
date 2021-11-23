03d4ab4e8e20577708fecb096dc5a37b4fd9b54e
jotacib3 <jotacib3@gmail.com>
2021-11-22 23:40:30 -0500

    feat(src/app): add authentication and admin dashboard to routes

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -3,11 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 import MainLayout from './layouts/main-layout';
-
+import DashboardLayout from './layouts/dashboard-layout';
 import HomePage from './views/pages/HomePage';
 import NotFoundPage from './views/pages/NotFoundPage';
+// import ProtectedRoute from './components/protected-route';
+import ProtectedOutlet from './components/protected-outlet';
 
+const DashboardPage = lazy(() =>
+  import('./views/dashboard/dashboard-default-content'),
+);
 const AboutPage = lazy(() => import('./views/pages/AboutPage'));
+const AuthPage = lazy(() => import('./views/pages/auth/AuthPage'));
 
 export const AppRoutes = () => {
   return (
@@ -26,7 +32,41 @@ export const AppRoutes = () => {
           }
           exact
         />
-
+        <Route
+          path={'/login'}
+          element={
+            <Suspense fallback={<LinearProgress style={{ margin: '10rem' }} />}>
+              <AuthPage />
+            </Suspense>
+          }
+          exact
+        />
+        <Route path="/dashboard" element={<ProtectedOutlet />}>
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense
+                fallback={<LinearProgress style={{ margin: '10rem' }} />}
+              >
+                <DashboardLayout>
+                  <DashboardPage />
+                </DashboardLayout>
+              </Suspense>
+            }
+            exact
+          />
+          <Route
+            path={'/dashboard/departments'}
+            element={
+              <Suspense
+                fallback={<LinearProgress style={{ margin: '10rem' }} />}
+              >
+                <AboutPage />
+              </Suspense>
+            }
+            exact
+          />
+        </Route>
         {/*eager loading*/}
         <Route path="*" element={<NotFoundPage />} exact />
       </Route>
