51ec5ae7310ac3e399233d7f2b530dcac675c0d0
jotacib3 <jotacib3@gmail.com>
2021-11-24 12:31:51 -0500

    refactor(src/store): add `categoryActionTypes` file

---
+++ src/store/product/productActionTypes.js
@@ -0,0 +1,8 @@
+export const productNamespace = 'product';
+
+export const ProductActionTypes = {
+  FETCH_PRODUCT: `${productNamespace}/FETCH_PRODUCT`,
+  POST_PRODUCT: `${productNamespace}/POST_PRODUCT`,
+  UPDATE_PRODUCT: `${productNamespace}/UPDATE_PRODUCT`,
+  DELETE_PRODUCT: `${productNamespace}/DELETE_PRODUCT`,
+};
