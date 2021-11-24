5d085f28cfd973f3c5487cc85b0a77c2163147d0
jotacib3 <jotacib3@gmail.com>
2021-11-24 12:52:29 -0500

    refactor: add `Results.jsx` component to product list view

---
+++ src/app/views/dashboard/product/ProductListView/Results.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MaterialTable from 'material-table';
+import tableIcons from '../../../../components/Table/table-icons';
+
+const Results = ({ columns, data, addRow, editRow, deleteRow }) => {
+  return (
+    <MaterialTable
+      title="Products"
+      icons={tableIcons}
+      columns={columns}
+      data={data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            addRow(newData);
+            resolve();
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            editRow(newData);
+            resolve();
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            deleteRow(oldData);
+            resolve();
+          }),
+      }}
+    />
+  );
+};
+
+Results.prototype = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired,
+  addRow: PropTypes.func.isRequired,
+  editRow: PropTypes.func.isRequired,
+  deleteRow: PropTypes.func.isRequired,
+};
+
+export default Results;
