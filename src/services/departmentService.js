6c13acc0b0c35b70bd48c924446826b9461eb780
jotacib3 <jotacib3@gmail.com>
2021-11-24 11:17:15 -0500

    refactor(src/services): change functions name in `departmentService.js` file

--- src/services/departmentService.js
+++ src/services/departmentService.js
@@ -4,14 +4,14 @@ export async function getDepartments() {
   return await api.get(EndPoints.departments);
 }
 
-export async function postDepartments(department) {
+export async function postDepartment(department) {
   return await api.post(EndPoints.departments, department);
 }
 
-export async function putDepartments(department) {
+export async function putDepartment(department) {
   return await api.put(`${EndPoints.departments}/${department.id}`, department);
 }
 
-export async function deleteDepartmentsById(id) {
+export async function deleteDepartmentById(id) {
   return await api.delete(`${EndPoints.departments}/${id}`);
 }
