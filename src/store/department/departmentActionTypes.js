9bd12eb620d0d396a28289e0e18dfe30456d14ee
jotacib3 <jotacib3@gmail.com>
2021-11-24 11:18:20 -0500

    refactor(src/store): create `departmentActionTypes` file

---
+++ src/store/department/departmentActionTypes.js
@@ -0,0 +1,8 @@
+export const departmentNamespace = 'department';
+
+export const DepartmentActionTypes = {
+  FETCH_DEPARTMENT: `${departmentNamespace}/FETCH_DEPARTMENT`,
+  POST_DEPARTMENT: `${departmentNamespace}/POST_DEPARTMENT`,
+  UPDATE_DEPARTMENT: `${departmentNamespace}/UPDATE_DEPARTMENT`,
+  DELETE_DEPARTMENT: `${departmentNamespace}/DELETE_DEPARTMENT`,
+};
