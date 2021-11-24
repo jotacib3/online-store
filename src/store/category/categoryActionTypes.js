15f24b99be019c6da1cbaed559d1a591d7b2a5c9
jotacib3 <jotacib3@gmail.com>
2021-11-24 11:10:13 -0500

    refactor(src/store/category): add `categoryActionTypes.js` file

---
+++ src/store/category/categoryActionTypes.js
@@ -0,0 +1,8 @@
+export const categoryNamespace = 'category';
+
+export const CategoryActionTypes = {
+  FETCH_CATEGORY: `${categoryNamespace}/FETCH_CATEGORY`,
+  POST_CATEGORY: `${categoryNamespace}/POST_CATEGORY`,
+  UPDATE_CATEGORY: `${categoryNamespace}/UPDATE_CATEGORY`,
+  DELETE_CATEGORY: `${categoryNamespace}/DELETE_CATEGORY`,
+};
