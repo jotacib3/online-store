7a58b685c266edd0a6a58b9975b5a9a658bcd93c
jotacib3 <jotacib3@gmail.com>
2021-11-24 13:06:15 -0500

    feat(src/app): add dashboard and crud pages to `routes.jsx`

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -6,11 +6,19 @@ import MainLayout from './layouts/main-layout';
 import DashboardLayout from './layouts/dashboard-layout';
 import HomePage from './views/pages/HomePage';
 import NotFoundPage from './views/pages/NotFoundPage';
-// import ProtectedRoute from './components/protected-route';
 import ProtectedOutlet from './components/Route/protected-outlet';
 
 const DashboardPage = lazy(() =>
   import('./views/dashboard/dashboard-default-content'),
+);
+const DashboardDepartmentPage = lazy(() =>
+  import('./views/dashboard/department/DepartmentListView'),
+);
+const DashboardCategoryPage = lazy(() =>
+  import('./views/dashboard/category/CategoryListView'),
+);
+const DashboardProductPage = lazy(() =>
+  import('./views/dashboard/product/ProductListView'),
 );
 const AboutPage = lazy(() => import('./views/pages/AboutPage'));
 const AuthPage = lazy(() => import('./views/pages/auth/AuthPage'));
@@ -61,7 +69,35 @@ export const AppRoutes = () => {
               <Suspense
                 fallback={<LinearProgress style={{ margin: '10rem' }} />}
               >
-                <AboutPage />
+                <DashboardLayout>
+                  <DashboardDepartmentPage />
+                </DashboardLayout>
+              </Suspense>
+            }
+            exact
+          />
+          <Route
+            path={'/dashboard/categories'}
+            element={
+              <Suspense
+                fallback={<LinearProgress style={{ margin: '10rem' }} />}
+              >
+                <DashboardLayout>
+                  <DashboardCategoryPage />
+                </DashboardLayout>
+              </Suspense>
+            }
+            exact
+          />
+          <Route
+            path={'/dashboard/products'}
+            element={
+              <Suspense
+                fallback={<LinearProgress style={{ margin: '10rem' }} />}
+              >
+                <DashboardLayout>
+                  <DashboardProductPage />
+                </DashboardLayout>
               </Suspense>
             }
             exact
