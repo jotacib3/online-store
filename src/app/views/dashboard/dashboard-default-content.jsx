01c0dbcf0be33b9103336dbed53d52c220bc3715
jotacib3 <jotacib3@gmail.com>
2021-11-24 13:36:55 -0500

    style(views/dashboard): remove unused elements in `dashboard-default-content.jsx` component

--- src/app/views/dashboard/dashboard-default-content.jsx
+++ src/app/views/dashboard/dashboard-default-content.jsx
@@ -1,14 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import {
-  Box,
-  Card,
-  CardContent,
-  Container,
-  Grid,
-  Typography,
-  useMediaQuery,
-} from '@material-ui/core';
+import { Box, Container, Typography, useMediaQuery } from '@material-ui/core';
 
 import Page from '../../components/page';
 
@@ -42,77 +34,3 @@ const useStyles = makeStyles(() => ({
     minHeight: '100%',
   },
 }));
-
-const getChartStyling = theme => ({
-  chart: {
-    background: theme.palette.background.paper,
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: ['#13affe', '#fbab49'],
-  dataLabels: {
-    enabled: false,
-  },
-  grid: {
-    borderColor: theme.palette.divider,
-    yaxis: {
-      lines: {
-        show: false,
-      },
-    },
-  },
-  legend: {
-    show: true,
-    labels: {
-      colors: theme.palette.text.secondary,
-    },
-  },
-  plotOptions: {
-    bar: {
-      columnWidth: '40%',
-    },
-  },
-  stroke: {
-    show: true,
-    width: 2,
-    colors: ['transparent'],
-  },
-  theme: {
-    mode: theme.palette.type,
-  },
-  tooltip: {
-    theme: theme.palette.type,
-  },
-  xaxis: {
-    axisBorder: {
-      show: true,
-      color: theme.palette.divider,
-    },
-    axisTicks: {
-      show: true,
-      color: theme.palette.divider,
-    },
-    categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    labels: {
-      style: {
-        colors: theme.palette.text.secondary,
-      },
-    },
-  },
-  yaxis: {
-    axisBorder: {
-      show: true,
-      color: theme.palette.divider,
-    },
-    axisTicks: {
-      show: true,
-      color: theme.palette.divider,
-    },
-    labels: {
-      style: {
-        colors: theme.palette.text.secondary,
-      },
-    },
-  },
-});
